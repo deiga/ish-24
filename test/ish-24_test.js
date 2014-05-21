8419080d785bb9f614875ff5a759e47cd886e274
Timo Sand <timo.j.sand@gmail.com>
2014-05-21 09:48:11 +0300

    Commented out test

--- test/ish-24_test.js
+++ test/ish-24_test.js
@@ -3,10 +3,10 @@
 var ish24 = require('../lib/ish-24.js');
 var assert = require('should');
 
-describe('ish24', function () {
+// describe('ish24', function () {
 
-  it('should be awesome', function () {
-    ish24.awesome().should.equal('awesome');
-  });
+//   it('should be awesome', function () {
+//     ish24.awesome().should.equal('awesome');
+//   });
 
-});
+// });
